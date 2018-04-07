b7b448cf2b6a630444bc6cb19957c41f13920f3a
Student <student@howard06.istlabs.rit.edu>
2018-04-07 17:30:09 -0400

    Added UI Skeleton

---
+++ Happy_Hour/Happy_Hour/ViewControllers/InfoViewController.swift
@@ -0,0 +1,14 @@
+//
+//  InfoViewController.swift
+//  Happy_Hour
+//
+//  Created by Student on 4/7/18.
+//  Copyright © 2018 DeSimone.Federico. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class InfoViewController: UIViewController {
+    
+}
---
+++ Happy_Hour/Happy_Hour/ViewControllers/ReviewTableVC.swift
@@ -0,0 +1,50 @@
+//
+//  ReviewTableVC.swift
+//  Happy_Hour
+//
+//  Created by Student on 4/7/18.
+//  Copyright © 2018 DeSimone.Federico. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ReviewTableVC : UITableViewController {
+    
+    @IBAction func unwindWithCancelTapped(segue: UIStoryboardSegue) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "simpleCell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let indexPath = tableView.indexPathForSelectedRow{
+            let selectedRow = indexPath.row
+            let infoVC = segue.destination as! InfoViewController
+        }
+    }
+    
+    @objc func addItem() {
+        performSegue(withIdentifier: "addReviewSegue", sender: nil)
+    }
+}
